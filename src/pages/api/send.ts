d049348c9773ef346e3631495a18bc0990e5e325
Ignacio Molina <molinagranda@gmail.com>
2025-08-06 18:21:38 -0300

    Creacion del Email

--- src/pages/api/send.ts
+++ src/pages/api/send.ts
@@ -1,16 +1,26 @@
 import type { APIRoute } from "astro";
 import { Resend } from "resend";
+import { render } from "@react-email/render";
+import SampleEmail from "../../emails/sampleEmail";
 
 const resend = new Resend(import.meta.env.RESEND_API_KEY);
 
 export const GET: APIRoute = async () => {
-  //Enviar Email
+  //Crear Email
+
+  const emailContent = SampleEmail({ username: "Nacho" });
+  const html = await render(emailContent);
+  const text = await render(emailContent, { plainText: true });
+  
+  
+    //Enviar Email
 
   const { data, error } = await resend.emails.send({
     from: "YOUR_NAME <YOUR_EMAIL@YOUR_DOMAIN>",
     to: ["delivered@resend.dev"],
     subject: "It works!",
-    html: "<strong>It works!</strong>",
+    html,
+    text,
   });
 
   if (error) {
